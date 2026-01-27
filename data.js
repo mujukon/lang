0e09d1c0207b4ece864922dc76b52b366165ea86
矛盾と混沌 <144119243+mujukon@users.noreply.github.com>
2026-01-27 16:33:00 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,186 @@
+var sentenceText = `*アクァルヅァルゴ語
+efntapqaot eʕd͡zallao	私は 窓を 開けて 外の 空気を 入れる
+epcʰisʼleq eɬvsitɲcʰeː	彼の 説明は 分かりやすかった ので 私は 完全に 納得できた
+ɲaɟem t͡saba elptak ðebtʼɪɲga	ペットの 鳥を 放すと 飛んでいってしまう かもしれない
+tato ejat ɪvt͡ʃo epyɲɣʷe	お父さんが 家に 帰ってくると 子供たちは みんな お父さんに 抱きついた
+d͡zo pʼara etʼm̥at t͡saba etqe	子供は 紙を 折って 鳥を 作った
+ivtølnɨstri uʔgjo kɳerkʃiɬora	世界で 起きている 戦争の 部分的な 原因は 領土問題である と 言われている
+amrqisʼqaθθɨgja esʼlãː	私たちは プロジェクトの 続行を 中止する ことに した 理由を 説明する
+aqincʼøʂmo erχa	この 町で 暮らすのは 平和である
+racʰo letɪilzazam epcam patɑː	医療は 人の 健康を 守る 大切な 仕事である
+ɣəd͡zelʒamkeo	試合に 負けて とても 残念な 気分であった
+eɬatʕopca	悪い 人を 罰する ことは 大切である
+ɬestmɨqʰapʰjaq qyɬɚ ellat mar̥is eqʰaː	食べ残しの 料理は 捨てるのが もったいない ので 冷凍庫に 入れて 後日 食べる
+nd͡zo eðɯfθaʔgar	赤ちゃんは 手足を 動かしている
+m̥ilo itɪikle ɣeɹkat epca	宗教は 人々の 生活に 深く 関わっていて 大切な 存在である
+uvʁɟɛ̃ːʒɾbo et͡ʃʎar	銀の 指輪が 光っている
+ɣekʔam ɬaʔo ŋenjaq ʂeqʰeːx	真っ赤な りんごは おいしそう なので 食べたくなった
+afriflɔŋ̊ vaɮəː	太陽の 光が あれば 影が ついてくる
+ŋaʔoo ɣeqχsaɖvat eqχsaqʰfrak eɬtnaç	私の 妹は 野菜が 大嫌いで 食卓に 野菜が 出ると 文句を 言う
+ɣefxet efsoɭmɳe	風が 強く 吹いて 木の 葉が 舞った
+djo ɤspo bɑːɾon evtʰiʃkʰar	男の子が 遊び場で 友達と 元気に 走り回っている
+tɑvŋo eʈat esøpɹa	都会に 住む 人は 多くて 便利である
+ʊpçɨpʰɲidʒis ecam ʈano ʈaʁœ̃r et͡ʃʼʷəʃkʰer	寒い 冬の 日に 小さな 電車は 線路の 上を 揺れながら 走っていた
+po t͡ɬʰeplta	彼は 話し合いが 上手な 人である
+epʰqøspesʈ mamo levderm ɬultʰa etʼm̥et eje	公園で 遊んだ 後 母は 広げていた シートを 畳んで 家に 帰った
+d͡zo et͡ʃʼvam xɨt͡ɬʼca eɖve	子供は 無理な 宿題を 嫌がった
+ɑɨɖəːk ekraɲɟaxt eħfax	恋人が いると 人生が 楽しくなり 幸せを 感じる ことが できる
+end͡zoʁlar	赤ちゃんが 泣いている
+pon çotʰndaleot eqt͡ʃʰeo	彼と 一緒に 映画を 見る 都合が つかず 残念であった
+ekʼekjaʃqʼaor	私は 木に 絵を 彫っている
+tato θakjy exrge	父は 仕事の 出張で 東京へ 行った
+θakjil mame epltʰeç	東京から よく 母へ 電話を かけていた
+ɲarsyl emrqeo	夏休みの 計画を 立てた
+nʊkʃøːm vaɮɢʷyl binθis eðnao	私は 来週の 試験に 向けて 毎晩 勉強を している
+epltẽr	彼らは 議論を 交わしていた
+awapclo akraɲɟyːmʐɨfθn̥ɑː	自分を 大切に する ことは 人生を 楽しむ ための 基本的な 条件である
+mame eʈʼpsaː	私は 母に メールを 送る
+mo œktliɣø tʰɑɲel usezaq keðnao	私の 成績は 後輩たちに 劣っている ので もっと 勉強しなければ ならない
+eɲɟɯçmt͡ʃʰam karo eza	楽しい ユーモアが ある 人生は 素晴らしい
+askøŋŋɨfɹχiflo lepjeʁbe	夕日が 空に 長く 延びている 光が 美しい 風景を 作り出した
+utlzœːstʼɨʕtʼo ɣekjaka	テレビの 上に 立っている アンテナが 映像を よく 受信する
+id͡zo eðbyçlãsm fiɬa eχqʼʷəspãr	子供達は 地面に 落ちた 木の 実を 拾って 遊んでいる
+ketʼqisθlaɮɮai	料理の メニューを 指定しなければ ならない
+raʁoəʔɪ atniʁoəʔ ɣeɣʷmɢɪɭɹɦe	彼女の 所作と 言葉づかいは 非常に 上品で 品位が あった
+ɲico eɪɟʁẽt ezlɢispẽ	兄弟は 契約を 結んで 仲良く 遊んだ
+rarim ɪvsʼɨmko kʰeːsʼqʃa	私の 両親の 財産は 全て 相続される ことに なっている
+ibɾoŋ̊ʂɬa epclaːç	私は 友達の 批判を 大切に する
+d͡zo pʰɤqø edɨmʁalpɲel	子供が 公園で 大きな ボールを 跳ね回していた
+xupø uχqɯkʔɨɟkʼʷa evlam mjəː	かわいい 赤い 髪リボンを つけた 女の子が いる
+ʃuŋt͡sʼu ɲescʼʉʃɹɨʔgaq çetʼastpʰsa	新幹線は 高速で 都市間を 移動できる ので 時間を 有効に 使える
+hʊdis etlzaleo	私は 昨日 テレビを 見た
+epçar̥rq biʒis esχʈʰavlaːr̥r	寒さが 長引いて コートを 着たまま でいる 日が 続いている
+eɖɹopca	家族が 大切である
+apcɨvɮɢʷœs ɬet͡ʃvaq məɣeðna	大事な 試験の 前は 油断すると よくない ので しっかり 勉強しよう
+amtɨrɣʷɨmbɹa exaː	朝の 鐘が 鳴り響く 音を 聞く
+ɪħaqqʰo eχqam ʒaɢøːt eħkac ɲeqʰaː	その お弁当は 可愛い 容器に 入っていて 歩きながら 食べられる
+mamo ɨʒa eççɪħkar	母は 荷物を 引っ張って 歩いている
+magjem alʁɢʷbɛːɲyl biʒis ekʼʷeç	彼女は アナウンサーに なる ことが 夢で 毎日 練習に 励んでいた
+afriɹo eħkarlo	太陽神が 歩く
+mo qʼaɹptʰa olaː	私は 虹を 見なかった
+mo fa exkɤː	私は あなたに 会う であろう
+ʔikʼa tʰe eqfreoh	魂を 持つ 物体を 霊に 差し上げた
+cat͡sʰu kyka ekɬeː	ナイフで 被り物を 切り裂いた
+ŋɛ!	猫よ
+ʃɑɬo ʒdiːrɛɪnalo ka eʒʒeɾfis	泥棒は 私の 主人から 金銭を 奪ってしまった
+ɖɯ kɯ ʔaɾo bakʼɑː	愛と 金と 両方 ガラクタである
+ʃɔmor sɑːkʰno eːɢʷẽ	店長によって 客たちが 追い出された
+imo qʰyl ʈʼekraõr	我々は 食事の ために 生きている のだ と 思う
+vaŋʷel abʒɨfsn̥o errça	殴打より 濡れ タオルが 効果的である
+mim qaɾo eɟʔe	私の 車が 壊れた
+fĩ m̥ɚ mjaqa nellaː	あなたの 目の 中に ネギを 入れよう
+miŋ θano era	私には 携帯が ある
+eŋkʁalʈ pĩ ʔako orex	何度も 横転した 後 彼には 顔が なくなった
+qo…mẽq tɨqɑː!	これは 私の 作品である
+aʃʁɨt͡spem latʰem qaɾo ere	白い 鳥であり 天使である 乗り物が あった
+ɣʷɑlnes raka erlɤrke	王として 地上を 支配する であろう
+zʌx ʔala eɣgaç	水の 代わりに 酒を 飲んでいる
+gartʰʌ evrer	悪魔の ように 笑っていた
+eɬam zɑdɛg eɲeis ə	悪い 大人に なった
+aziqʼom ŋanor eːcʰmai??	蛇口の ような 蛇に 噛まれた のか
+ʕaɟɣvɛd ɤcal etoscaɹ	不況の もと 職場から 人が 減っている
+fʉn ramɣʷo kʷɑː?	あなたに とって グリフィンは どの ような ものか
+akʃisɬɑx so lavɮɑː	週刊誌に よると それは 陰謀である
+qʰat͡ɬxɛ̃k ɮaga nəlet͡saː	チョコレートで できた 城を 暖めよう
+aʂçɨt͡ʃɯʔ renja	抹茶なので おいしい であろう
+ɾʊdis ɬaqʰy mega	今日は 祭りに 行こう
+so rʊmprœs ore	それは ローマ帝国 時代 以前には なかった
+raʕtʼœs qo neqɲax	今 この 瞬間 より 後 これは 国有と する
+xʷaʈɔŋ̊ vt͡ʃʼagɦallo ma elqʼeɾos	春と 共に 花粉症が 私を 襲う
+fatʰil eɢʷo	布団から 出ろ
+zɤty ege	敵陣に 行った
+po tʰɤndø eːʒdɹnes	彼は 映画館で 逮捕された
+χan̥fus paɳu ɲegai	海王星まで 船で 行ける
+baʁœ̃r meŋiχqilda	橋の 上で 狂った ように 踊ろう
+faθɤ̃rih ʒaʔtʼo era ɔ	あなたの 足の 下に 懐中時計が ある
+jœ̃o ŋazo ere	私の 家の 横に 川が あった
+tjakɔ ʂakpʼŋa efneː	的に 向かって ショットガンを 撃った
+ʁɔs d͡zɑːʐɖŋʷo eɳbar	道に 沿って かつて 人間で あった 人形が 並んでいる
+fo camtʰœz estʼeir?	あなたは 怪異の 前に 立っていた のか
+yqʷiz hja etqo	冷蔵庫の 後ろに 空間を 作っておきなさい
+so ʔiʔʉ ebʐar?	それは AIたちの 間で 流行っている のか
+ʈanɚ to orap	電車の 中に 人が いない
+tʼɤmøg ɪcamt͡ʃo era ɔ	駅の 近くに その 事故物件は あった のだ
+adjoːɹkʰeɬ epʰteːr	ケモノの 少年から 離れた ところで スポーツを していた
+ɾis ma mɑloŋ fetŋejo?	今 私を 魔法使いと 呼んだ のか
+fo falov ɲelaje?	あなたは 光なしで ものを 見る ことが できる のか
+ɤŋʷɨtʼmo eːtqar	新しい 駅が 建設されている
+efrçkʼalaok ɦʌʂ evbjeox	夕焼け 空を 見上げると 心が すっと 穏やかな 気持ちに なった
+aðʒʷɨlɲɟəːõ	私たちには 共通の 目標が ある
+jo hɤfɑːyɾt qartʼɑːyɾ	家は 安らぎの 場所であり 私たちの 心の 拠り所である
+fɯʒisãi eɣjdam kja eʕɹkeː	富士山の 雄大な 眺めに 感動した
+d͡zis babor afpʰiʁa eːktleːŋ oʒvɤː	子供の 頃 祖父から 笛を 吹く ことを 教えてもらった 思い出は 忘れられない
+ɣeplɯsnovva	彼女の 言葉と 行動には 大きな 矛盾が ある
+eŋʷam maʃo orrçeq pʰɑve eχpleo	新しい 薬の 効果が あまり 良くなかった ので 医者に 相談した
+pano eŋʷam tɤqa leɣe	会社は 新しい 工場を 拡大した
+anθiskø pʰiɬpo eʁbat bo elẽr	夜空に 咲く 花火の 美しさに みんなが 見とれていた
+eʂʂɯm̥tʰøŋʁʷao	私は 肩と 背中が 痛い
+ʒamju eʒaʒmɾaq kʼemjaː ə	縄が あれば 荷物を しっかりと 縛る ことが できる ので 買っておきたい
+qɑne esʼχɑɟʁaõ	私たちは 国民に 正義を 誓う
+ɾʊdis ɨnθiqʰe ha evvɤ? qʰaxsɯ ŋaka evvɤ	今日の 夕食に 何が 必要か 野菜と 肉が 要る であろう
+œlzɨxʷeːɲ biʒis mepʰta	健康な 体重を 維持する ために 毎日 運動しよう
+esʼɨʃtaktaɲ ikʰtɨɹʔa kʼeɳʔisʼla	正確な 情報を 伝える ためには 確かな 事実を ていねいに 説明する 必要が ある
+pʰɤqø xanon ecʼmaor	公園で 犬を 散歩させている
+sat͡skis envɹam syt͡sa essaːç	洗濯物を 洗う ときは 環境に 優しい 洗剤を 使う ように している
+nd͡zo ɣʊmil evrer	赤ちゃんが 産まれた 時から 笑顔であった
+pʰɤqin çalfa lepʰat net͡sɨɣgatqaː	公園の 落ち葉を 焚き火に して 温かい 飲み物を 作ろう と 思う
+sako abmɯvdɨhjɑː	空は 青く 広い 空間である
+ʊlt͡ʃiqʰɑk bɑːɾon elt͡ʃiqʰaqʰyːʕdẽ	昼食の 時間に なった ので 仲間たちと 一緒に ランチを 食べに 外に 出かけた
+xapʰo et͡sə̃ɣgo	コーヒーが 冷めない うちに 飲もう
+ɣefroflam ɲasʼis d͡ʒis paɹin fyka essak epçɨfxa	太陽が 強く 照りつける 夏の 日に 部屋の 扇風機を 回すと 涼しい 風が 吹く
+qi eŋʷam kyʎi fykʰo ɣeɣat kasʼaɭ ɲəɣeʒʒa	この 新しい 掃除機に 付いている 吸引装置は 非常に 強力で ゴミも しっかり 取れる
+kɤmø eχqam ŋʷito kʼʉ ebtʼãr	動物園で 可愛い 猿が 木から 木へ 飛んでいる
+seqqʰyɖɣʷao	私は 海外旅行に 行きたい
+ðɤnil eçaoʔ et͡ʃçvan ɛkʼe efθçaot eʒŋkʼoðɲe	学校から 帰ったら うっかり 木の 枝に 足を 取られて つえが 折れてしまった
+eʃɹkʰe	彼は 速く 走った
+ʃoo asvmalɑːt gʊd͡ʒis ɾjoːɣoxkokɯɣikãy egaoç	相撲を 見る のが 私の 趣味で 休みの 日に よく 両国国技館に 行く
+d͡zo ðɤnø eðna	子供は 学校で 勉強する
+narzis zʊzis eʈaq ɣəzezgora	最近は 雨の 日が 多い 天気で 傘を 持っていれば 本当に 助かる
+sala emjejet eɟŋo	本を 買ったら 郵送で 送ってほしい
+ɾisa ʃar̥xa ɣeʃat ɖaɹon ɤnriːʃmy egaç	リサは アイスクリームが 大好きで いつも 家族と 一緒に 近くの 店に 行く
+kʰɑqʉ epleo	私は 外国人に 混ざって 話した
+bimtis ɣaʁʷes laqʼʷe esmtʰam avzbixʷfa eccʰaːt d͡ʒa epʰmaːç	毎朝の 習慣で 肌に 優しい 温泉ガスを 浴びて 一日を 始めている
+mamo vaqʰil ɳaɖʐɦo etqe	お母さんは 粉で クッキーを 作った
+sʼaɬo d͡zis kʼeʒam epcam baʈʼo	本を 読む のは 子供の とき 身につけるべき 大切な 習慣である
+qi nago eŋ̊ʷat onja	この パンは 古くて おいしくない
+ɟaɣʷa esɬɪɟlzeː ɳɨʃta zeka	資料を 読むと よく 分かった 新しい 知識が 得られる のは 良い ことである
+raʕtʼis epʰɾam spɛklɑːo	私は 目下 アルバイトを している 大学生である
+bɤɣlø eːlŋ̊ʷam baɣla emjaːt eŋ̊ʷam mɪza eɲɟaː	私は レコード屋で 昔 懐かしい レコードを 買って 昔の 曲を 楽しんでいる
+si tim pata ɢʷelɣŋaː	その 人の 仕事を 評価する のは 難しい
+ecaop adɨmgjəː	私は 身長が 小さい が 大きな 夢を 持っている
+*ŧaq語
+łe cof eproäçeqá	昨日の 会議では 大切な 問題を 話し合った
+tăs'fem ejăilam	駅の 案内所で 道を 案内してもらった
+edătiářuŧa elăf'copusa	私は 不要な データを 削除して コンピューターの 容量を 確保した
+kaaldăȟ	彼は 本来 優しい 人である
+eȟom'ȟekus đepadič	コンサートの チケットを 手に 入れた ので 客席の 良い 場所を 確保しなければ ならない
+sozm đaapčäđo	夏休みは あと 5日 しか 残っていない
+äpidäftároǧ raär'äfel	実用性の 高い 新しい 製品が 出ると 便利に なる ことが 多い
+mim šät đăvep	母は シャツに アイロンを かける
+gomä gaäriniz	その 大会には 多くの 団体が 参加して 競い合う
+ňăm'ňemáx ňimso	小数点は 整数と 小数の 間に ある
+fe epacełaq	風船は 空中を 優雅に 舞っていた
+bakăł bonăǧ ezi'bank'beňuxad	カウボーイは 銃撃戦で 相手の ピストルの 弾丸を うまく 避けた
+hop emăex	赤ちゃんは 病院で 産まれた
+deasmej'ăti äzămiŧa reał'oatođa a'iđam	恥ずかしい 過去を 知っている やつらの 記憶を 消させて 迷惑を かけて ごめん と 言っている 誰か 助けて
+iarňu 'iarsi dă ibăera raej'iejeŧa e'upam	興味が ない こと 本気で ない ものは 全部 後回しで 知っている ことは 知らない ふりを する 私は 終わっている
+nuxi äloetok 'ežu änoxus'iaf óxăer, aAlis qă e'ig	そこで アリスは 頭の なかで ひなぎくの くさりを 作ったら 楽しい であろう が 起きあがって ひなぎくを 摘む のも 面倒である ので どうしようか と 考えていた
+paku ifakoi isiläv, aAlis e'ig	絵や 会話の ない 本など 何の 役にも 立たない と アリスは 思った
+ho do naŧesi selesän kă paku ifako	一 二回は おねえさんの 読んでいる 本を のぞいてみた が そこには 絵も 会話も ない
+Alis voáf enăŧásuž ye'ero e'ubalip	アリスは 川辺で おねえさんの よこに 座って 何も する ことが ない ので とても 退屈しはじめていた
+yéłăxăqařalumasé	あなたたちは くっついた とても 黄色い バターの 代替物を 少しも 好きに なる ことが できなかった
+beobi calämu dugär soásk ähok'gebäciŧ	世界に 不満を 持っている 2匹の 海老と 沢山の 戦士たちが 斜め 後ろの 店で いくつかの ヤスリ 歯磨きの 端っこを 壊している であろう
+*イスクイル3
+Uiphawâtļûxe’ň?	それは 象徴的な 大きな 木立 なのでしょうか?
+hremsoqaiţsurkoi	人間の 多様な 属性から 得られる 価値によって
+／qhûl-lyai’svukšei’arpîptó’ks	抵抗にもかかわらず 怪しげな ピエロの 寄せ集めの集団を 使って 繰り返し 恐怖を煽ろうとした と伝えられているが 信じ難い
+Tî akt’asalb abjatļud	私の ホバークラフトは ウナギで いっぱいです
+Ki glawulač?	調子は どう?
+En-n ki ač	あなたが 元気でいることを 願っている
+Ti glalač	私は 元気だ
+Ükhu attál	挨拶を申し上げます
+Ki attàwîl	あなたに 挨拶を
+Attál	こんにちは
+Attàwîl	こんにちは
+Attàlûk	さよなら
+Ükhu ettál	歓迎します`

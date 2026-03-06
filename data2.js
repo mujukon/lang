24524c09ccedf812c501bbfa7196c16e564b8f4e
矛盾と混沌 <144119243+mujukon@users.noreply.github.com>
2026-03-06 22:27:54 +0900

    data2.js の作成

---
+++ data2.js
@@ -0,0 +1,580 @@
+var sentenceText = `*ハワイ語機能語
+>代名詞
+au 私
+ʻoe あなた
+ʻo ia 彼 / 彼女 / それ
+māua 私たち2人（除外）
+kāua 私とあなた（2人）
+mākou 私たち（除外）
+kākou 私たち（包括）
+ʻolua あなたたち2人
+ʻoukou あなたたち
+lāua 彼ら2人
+lākou 彼ら
+>冠詞
+ke 定冠詞（特定名詞；k/e/o/aで始まる語に多い）
+ka 定冠詞（特定名詞；それ以外の語に多い）
+nā 定冠詞（複数）
+he 不定冠詞 / 属性述語
+kekahi ある一つの / ある～
+kēlā あの
+kēia この
+ua 両方の
+>前置詞
+i ～へ / ～に / ～で（場所・方向）
+ma ～に / ～で（位置）
+mai ～から
+a ～の（人・関係）
+o ～の（所有・関係）
+no ～のための / ～の
+na ～によって / ～のもの
+me ～と一緒に
+me he ～のような
+me ka ～と共に
+>方向語
+aku 話し手から離れる方向へ
+mai 話し手の方へ
+aʻe 上へ・外へ・さらに
+iho 下へ・内へ
+>TAM
+ua 完了 / 過去
+ke … nei 進行 / 現在
+e … ana 未来 / 未完了
+i 過去 / 完了（叙述文）
+e 命令 / 未来的行為
+mai 禁止命令
+aneʻi ここへ（方向・近接）
+aku 向こうへ（話者から離れる）
+mai 話者へ向かう
+aʻe 上へ / 外へ / さらに
+iho 下へ / 内へ
+>不可分な所有形
+koʻu 私の
+kou あなたの
+kona 彼 / 彼女の
+ko kāua 私とあなた（2人）の
+ko māua 私たち2人（聞き手除く）の
+ko kākou 私たち（聞き手含む）の
+ko mākou 私たち（聞き手除く）の
+ko ʻolua あなたたち2人の
+ko ʻoukou あなたたちの
+ko lāua 彼ら2人の
+ko lākou 彼らの
+>譲渡できるものの所有形
+kaʻu 私の
+kāu あなたの
+kāna 彼 / 彼女の
+ka kāua 私とあなた（2人）の
+ka māua 私たち2人（聞き手除く）の
+ka kākou 私たち（聞き手含む）の
+ka mākou 私たち（聞き手除く）の
+ka ʻolua あなたたち2人の
+ka ʻoukou あなたたちの
+ka lāua 彼ら2人の
+ka lākou 彼らの
+*ハワイ語単語
+>1
+a	そして（文をつなぐ接続詞）
+a	〜の（所有：a-class）
+ʻae	はい；同意する；承認する；貸す
+aʻe	上へ；上向きに
+aha	何
+ahahana	恥ずかしい；みっともない
+ahea	いつ（未来の疑問）
+ahi	火；マグロ
+ahiahi	夕方
+a hiki i	〜まで
+ʻai	食べる；食べ物
+aia	〜がある；〜がいる
+ʻaihue	盗む；泥棒
+ʻaina	食事
+ʻāina	土地
+ʻaina ahiahi	夕食
+ʻaina awakea	昼食；ブランチ
+ʻāina ʻē	外国；異国
+ʻāina hānau	生まれ故郷
+ʻāina kakahiaka	朝食
+akā	しかし
+ʻakaʻaka	笑う
+ʻākala	ピンク色
+akamai	賢い；知恵
+ʻākau	右；北
+ʻako	摘む；折る
+aku	話し手から離れる方向へ
+akua	神
+ala	道；起きる
+ʻalani	オレンジ
+ala nui	大通り
+ʻalo	避ける
+aloha	こんにちは；さようなら；愛
+a me	〜と（名詞接続）
+āna	彼／彼女の
+ʻanakala	叔父
+ʻanakē	叔母
+ʻaneʻi	ここに；ここで
+ʻano	種類；やや
+anu	寒い
+ao	日中
+aʻo	教える；学ぶ
+aʻo aku	教える
+aʻo mai	教わる
+aoʻao	側；ページ
+ʻaʻohe	何もない
+ʻaʻole	いいえ；〜ない
+apau	全て
+apōpō	明日
+au	私
+aʻu	私の；私を
+auau	水浴びする；泳ぐ
+auau kai	海水浴する
+ʻauinalā	午後
+aumoe	真夜中
+awakea	正午
+ʻawapuhi	野生ジンジャー
+ʻāwīwī	素早い
+>2
+e	命令；呼びかけ
+aʻe	別の；他の
+ʻeha	痛む
+ʻehā	4
+ehia	いくつ
+ʻehiku	7
+eia	ここに〜がある
+ʻeiwa	9
+hoʻokahi	1
+eke	袋
+ʻeke kua	リュック
+ʻekolu	3
+ʻeleʻele	黒
+ʻelemakule	老人
+eli	掘る
+ʻelima	5
+ʻelua	2
+emi	下がる；安い
+ʻenuenue	虹
+ʻeono	6
+ʻewalu	8
+>3
+hāʻawi	与える
+haʻawina	課題；宿題
+haha	手探りする
+haʻi	言う
+hala kahiki	パイナップル
+hale	家
+hale ʻaina	レストラン
+hale pule	教会
+hamama	開いている
+hana	仕事；働く
+hānai	養う
+hānau	生む
+hānau mua	年上
+hānau hope	年下
+haneli	100
+haole	白人；外国人
+hapa	半分
+hāpai	運ぶ；妊娠する
+haumāna	生徒
+hauʻoli	幸せ
+haupia	ココナッツプリン
+he	不定冠詞
+hea	どこ
+heʻe	タコ
+heʻenalu	サーフィン
+heʻi	パパイヤ
+hele	行く
+hele mai	来る
+hele wāwae	歩く
+helu	数える；数
+heluhelu	読む
+helu kelepona	電話番号
+hema	左；南
+hiamoe	眠る
+hiapo	長子
+hiki	到着する；できる
+hilahila	恥ずかしい
+hīmeni	歌う
+hina	落ちる
+hoa aloha	友人
+hoahānau	いとこ
+hoa noho	同居人；隣人
+hoʻāʻo	試す
+hoʻauau	入浴させる
+hoe	漕ぐ
+hoʻi	戻る
+hoʻihoi	興味がある
+hoʻihoʻi	返す
+hōʻike	見せる；試験
+hoka	失敗する
+hola	時
+holoholo	散歩する
+holoi	洗う
+home	家
+hoʻolohe	聞く
+hoʻomaka	始める
+hoʻomākaukau	準備する
+hoʻouna	送る
+hope	後；最後
+hopena	結果
+hopena pule	週末
+hou	新しい；再び
+hua	果実
+huaʻai	果物
+huahelu	数字
+hua moa	卵
+hua ʻōlelo	単語
+huhu	怒る
+hui	団体；会社
+hulu	羽
+huna	隠す
+hupo	ばか
+>4
+i	〜に；〜で；〜を
+ia	彼／彼女
+iʻa	魚
+iaʻu	私を
+ihea	どこへ
+iho	下へ
+ʻike	見る；知る
+ʻike maka	目撃する
+i laila	そこに
+imi	探す
+inā	もし
+ināhea	いつ（過去）
+inoa	名前
+inu	飲む
+iole	ネズミ
+ipo	恋人
+ipu	容器
+iwakālua	20
+>5
+ka	定冠詞
+kaʻa	車
+kaʻa lawe ʻōhua	バス
+kāhea	呼ぶ
+kahi	場所
+kahiko	古い
+kahua mokulele	空港
+ikaika	強い
+ʻīlio	犬
+iwi	骨
+kahuna	神官
+kai	海
+kaikaina	弟／妹
+kaikamahine	娘
+kaikuaʻana	兄／姉
+kaikuahine	姉妹
+kaikunāne	兄弟
+kakahiaka	朝
+kakahiaka nui	早朝
+kākau	書く
+kākou	私たち（包括）
+kālā	お金
+kalaiwa	運転する
+kalaka	トラック
+kali	待つ
+kalua	地中焼き
+kamaʻa	靴
+kamaʻāina	地元の人
+kamaliʻi	子供
+kanaka	人
+kuke	料理する
+kukui uila	電灯
+kula	学校
+kula nui	大学
+kumu	先生
+kupuna	祖父母
+kuʻu	私の（愛称）
+koʻu	私の
+kou	あなたの
+kū	立つ
+kua	背中
+kūʻai	売買する
+kūʻai aku	売る
+kūʻai mai	買う
+kuawa	グアバ
+>6
+lā	太陽；日
+lā hānau	誕生日
+laiki	米
+laki	幸運
+lākou	彼ら
+lāua	彼ら（2人）
+lalo	下
+Lāpule	日曜日
+lawa	十分
+lawaiʻa	漁師
+lawe	持つ
+lawe aku	持っていく
+lawe mai	持ってくる
+leka	手紙
+leo	声
+lepo	土
+liʻiliʻi	小さい
+like	同じ
+lilo	〜になる
+lima	手
+lio	馬
+loa	とても
+loaʻa	得る
+lohe	聞こえる
+lōʻihi	長い
+loio	弁護士
+loko	内側
+lokomaikaʻi	親切
+lole	服
+lomi	マッサージする
+lua	穴
+luau	宴会
+lumi	部屋
+luna	上；上司
+luʻu	潜る
+>7
+ma	〜で
+mahalo	感謝する
+mahiʻai	農家
+mahina	月
+mai	〜から
+maiʻa	バナナ
+maikaʻi	良い
+maka	目
+makahiki	年
+makaʻi	警官
+makamaka hou	新しい友人
+makana	贈り物
+makani	風
+makapō	盲目の
+makaʻu	恐れる
+mākaukau	準備できた；熟練した
+make	死ぬ；死
+mākeke	市場
+makemake	欲する；望む；好む
+mākou	私たち（聞き手を含まない・3人以上）
+makua	親
+makuahine	母
+makua kāne	父
+māla ʻai	畑
+ma laila	そこで；そこに
+mālama	世話する；大事にする
+māla pua	花畑
+mālie	静かな；穏やかな
+malo	腰布
+maloʻo	乾いた
+maluhiluhi	疲れた
+māmā	ママ
+māmā kahu	牧師の妻
+manakō	マンゴー
+manaʻo	思う；考え；意見
+manawa	時間
+maneo	かゆい
+manō	サメ
+manu	鳥
+ma ʻō	あちらで；あちらに
+maopopo	分かる；理解する
+mau	いつも；継続
+māua	私たち（聞き手を含まない・2人）
+me	～と；～と共に
+mea	物；人
+mea ʻai	食べ物
+mea hoʻokani pila	音楽家
+mea inu	飲み物
+mea kanu	作物
+mea nui	大事なこと
+mea ʻono	お菓子；ケーキ
+melemele	黄色
+minamina	惜しい；残念
+moa	鶏
+moʻa	火が通った；焼けた
+moe	横になる；眠る
+mokulele	飛行機
+mokupuni	島
+moloā	怠け者の
+momona	太った；甘い
+moʻo	トカゲ
+moʻolelo	物語
+moʻopuna	孫
+mua	前；先
+muli loa	末っ子
+muʻumuʻu	ハワイ風ドレス
+>8
+nā	複数冠詞（the）
+naʻau	腹；心（比喩）
+naʻaukake	ソーセージ
+naʻau palupalu	優しい心
+naʻe	しかし
+nahu	噛む；虫刺され
+nalowale	消える
+nāna	彼／彼女のために；彼／彼女の
+nānā	見る；観察する
+nanea	くつろぐ；楽しむ
+nani	美しい
+nāu	あなたのために；あなたの
+naʻu	私のために；私の
+neʻe	動く；移動する
+nehinei	昨日
+nīele	詮索好き
+nīnau	質問する；質問
+no	～のための
+nō	本当に；確かに
+noho	住む；座る
+nō hoʻi	本当に；また
+nohona	生活；生活様式
+noi	求める；要求する
+no ka mea	なぜなら
+no laila	それゆえ
+nona	彼／彼女のために；彼／彼女の
+nou	あなたのために；あなたの
+noʻu	私のために；私の
+nuha	不機嫌
+nūhou	ニュース
+nui	大きい；多い
+nūpepa	新聞
+>9
+ʻo	～の；主語標識
+ʻō	フォーク
+ʻoe	あなた
+ʻohana	家族
+ʻoi	優れている
+ʻoiaʻiʻo	本当
+ʻo ia	彼；彼女
+ʻoihana	仕事；職業
+ola	生きている；生命
+ola kino	健康
+ʻole	～でない
+ʻōlelo	話す；言葉
+ʻōlelo haole	英語
+ʻōlelo Hawaiʻi	ハワイ語
+ʻōlelo noʻeau	ことわざ
+ʻolua	あなたたち（2人）
+ʻoluʻolu	親切な；快適
+ʻōmaʻomaʻo	緑
+ona	彼／彼女の
+one	砂
+ʻono	おいしい；好む
+ʻopihi	オピヒ（貝）
+ʻōpio	若い
+ʻōpū	胃；腹
+ou	あなたの
+ōʻu	私の
+ʻoukou	あなたたち（3人以上)
+>10
+pā	囲い；庭
+paʻa	閉まった；固い
+paʻakai	塩
+pāʻani	遊ぶ
+paha	多分
+pahi	ナイフ
+pahu hau	冷蔵庫
+pāʻina	パーティー
+pakahi	一つずつ
+pākaukau	テーブル
+Pākē	中国人
+pakini	たらい
+Pakipika	太平洋
+palaoa	パン
+palaoa palai	パンケーキ
+palai	揚げる；炒める
+palaunu	茶色
+pali	崖
+pālule	シャツ
+palupalu	柔らかい
+pane	答える；答え
+pani	閉める
+papa	クラス；板
+pāpā	パパ
+pehea	どう
+pēlā	そのように
+pelena	クラッカー
+peni	ペン
+penikala	鉛筆
+pepa	紙
+pia	ビール
+piha	満ちた
+pihoihoi	不安
+piʻi	登る
+pikake	ピーカケ
+pila	請求書
+pilikia	問題
+pinepine	しばしば
+pipi	牛
+pipi kaula	ビーフジャーキー
+pō	夜
+Pōʻaha	木曜日
+Pōʻakahi	月曜日
+Pōʻakolu	水曜日
+papa heʻe nalu	サーフボード
+papa ʻeleʻele	黒板
+pāpale	帽子
+Papa Ola	保健局
+pau	終わる
+peʻe	隠れる
+Pōʻalima	金曜日
+Pōʻalua	火曜日
+Pōʻaono	土曜日
+poʻe	人々
+pōhaku	石
+poi	ポイ
+poina	忘れる
+poke	切る
+pololei	正しい
+pololi	空腹
+polū	青
+pōmaikaʻi	幸運
+poni	紫
+pono	正しい
+ponoʻī	自身の
+poʻo	頭
+pōpoki	猫
+pū	一緒に
+pua	花
+puaʻa	豚
+puka	出る；穴
+puka aniani	窓
+puke	本
+Pukiki	ポルトガル人
+pule	祈る；祈り
+pulehu	焼く
+puna	スプーン
+puʻolo	包み
+pūpū	スナック
+pupuka	醜い
+pupule	狂った
+>11
+selamoku	ジーンズ
+tsa	ちぇっ
+tūtū	祖父母
+tūtū kāne	祖父
+tūtū wahine	祖母
+ua	完了；雨
+uahi	煙
+ʻuala	サツマイモ
+uē	泣く
+uʻi	美しい
+uila	電気
+uka	山側
+uku	支払う
+uku	ノミ
+ʻulaʻula	赤
+ʻuliʻuli	暗い色
+ulu	成長する
+ʻulu	パンの実
+ʻumikūmākahi	11
+wā	時
+waʻa	カヌー
+waena	間
+wahi	場所
+wahine	女性
+waho	外
+wai	水；誰
+waiho	置く
+waihoʻoluʻu	色
+wai huaʻai	フルーツジュース
+wailele	滝
+waiū	牛乳
+waiwai	裕福
+wale nō	～だけ
+wāwae	足
+wehe	開く
+wehewehe	説明する
+wela	暑い
+wīkini	酢
+wīwī	痩せた`
